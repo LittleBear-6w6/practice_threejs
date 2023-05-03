86bcc8a493961937bc462ddef864f35116b3c25b
LittleBear-6w6 <57203646+LittleBear-6w6@users.noreply.github.com>
2023-05-03 23:31:02 +0900

    メッシュをグループにまとる

--- secondstep/src/js/index.js
+++ secondstep/src/js/index.js
@@ -12,6 +12,10 @@ const init = () => {
   render.setPixelRatio(window.devicePixelRatio);
   render.setClearColor("#000000");
 
+  const camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
+  camera.position.z = 3;
+
+// -------  ベクトル作成 ------
   const vec = new THREE.Vector3(1,2,3);
   vec.x = 9;
   console.log(vec);
@@ -21,7 +25,38 @@ const init = () => {
 
   vec.setScalar(7);
   console.log(vec);
+// --------------------------
 
+// -------- 複数メッシュのグループ化 ------------
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshPhongMaterial({
+    color: "#ffffff",
+  });
+  const meshA = new THREE.Mesh(geometry,material);
+  meshA.scale.set(2,0.1,0.1);
+  const meshB = new THREE.Mesh(geometry,material);
+  meshB.scale.set(0.1,2,0.1);
+
+  const group = new THREE.Group();
+  group.add(meshA);
+  group.add(meshB);
+
+  group.rotation.x = Math.PI * 0.2;
+  group.rotation.y = Math.PI * 0.25;
+
+// ----------------------------------------
+
+  const light = new THREE.PointLight('#ffffff');
+    light.position.x = 0.5;
+    light.position.y = 0.5;
+    light.position.z = 2.5;
+
+    const scene = new THREE.Scene();
+    scene.add(group);
+    scene.add(light);
+    scene.add(group);
+
+    render.render(scene, camera);
 }
 
 if (document.readyState === "complete") {
