64d0e004d2205cf553ca7de5200f726b9a8b7ba6
LittleBear-6w6 <57203646+LittleBear-6w6@users.noreply.github.com>
2023-05-03 23:17:56 +0900

    Vector追加

--- secondstep/src/js/index.js
+++ secondstep/src/js/index.js
@@ -11,4 +11,21 @@ const init = () => {
   render.setSize(width, height);
   render.setPixelRatio(window.devicePixelRatio);
   render.setClearColor("#000000");
+
+  const vec = new THREE.Vector3(1,2,3);
+  vec.x = 9;
+  console.log(vec);
+
+  vec.set(4,5,6);
+  console.log(vec);
+
+  vec.setScalar(7);
+  console.log(vec);
+
 }
+
+if (document.readyState === "complete") {
+    init();
+  } else {
+    document.addEventListener("DOMContentLoaded", init);
+  }
